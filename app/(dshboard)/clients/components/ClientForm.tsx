5a3b8149c73acd3ed18e84ad78fc47283116bdee
rusu55 <rusu55@yahoo.com>
2023-11-10 20:41:50 -0600

    updated

--- app/(dshboard)/clients/components/ClientForm.tsx
+++ app/(dshboard)/clients/components/ClientForm.tsx
@@ -1,10 +1,161 @@
 'use client';
 
-import {Contro}
+import { useForm } from 'react-hook-form';
+import {format} from 'date-fns';
+import { Form, FormField, FormLabel, FormControl, FormMessage, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import {LuCalendarDays} from 'react-icons/lu';
 import {z} from 'zod';
+
+
 const ClientForm = () => {
+  const onSubmit = (data) =>{
+    console.log(data);
+  }
+
+  const form = useForm({})
+
   return (
-    <div>ClientForm</div>
+    <div className="py-2 pb-4">
+      <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+              <div className="flex w-full max-w-full items-center space-x-2 ">
+                  <FormField control={form.control} name='lastName' render={({field}) => (
+                    <FormItem className=' w-[300px]'>
+                      <FormLabel>Last Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Last Name" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )} />
+                  <FormField control={form.control} name='firstName' render={({field}) => (
+                    <FormItem className='w-[300px]'>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="First Name" {...field} />
+                      </FormControl>
+                    </FormItem>
+                  )} />
+              </div>
+              <FormField control={form.control} name='email' render={({field}) => (
+                    <FormItem className='max-w-[610px]'>
+                      <FormLabel>Email Address</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Email Address" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )} />
+                  <FormField control={form.control} name='phone' render={({field}) => (
+                    <FormItem className='max-w-[610px]'>
+                      <FormLabel>Phone Number</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Phone Number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )} />
+               <FormField control={form.control} name="weddingDate" render={({field}) =>(
+                            <FormItem className="flex flex-col mt-3">
+                                <FormLabel>Wedding Date</FormLabel>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                          <FormControl>
+                                            <Button
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-[240px] pl-3 text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                                >
+                                                {field.value ? (
+                                                    format(field.value, "PPP")
+                                                ) :(
+                                                    <span>Pick a date</span>
+                                                )}
+                                                <LuCalendarDays className="ml-auto h-4 w-4 opacity-50" />
+                                            </Button>
+                                          </FormControl>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                disabled={(date) =>
+                                                date > new Date() || date < new Date("1900-01-01")
+                                                }
+                                                initialFocus
+                                            />
+                                        </PopoverContent>
+                                    </Popover>                                
+                                <FormMessage />
+                            </FormItem>
+                        )} /> 
+                  <FormField control={form.control} name="services" render={() =>(
+                            <FormItem className="mt-3">
+                                <div className="mb-4">
+                                    <FormLabel className="text-base">Services</FormLabel>                                    
+                                </div>
+                                {services.map((service) => (
+                                    <FormField
+                                        key={service.id}
+                                        control={form.control}
+                                        name='services'
+                                        render={({ field }) => {
+                                            return(
+                                                <FormItem
+                                                    key={service.id}
+                                                    className="flex flex-row items-start space-x-3 space-y-0"
+                                                >
+                                                    <FormControl>
+                                                    <Checkbox
+                                                        checked={field.value?.includes(service.id)}
+                                                        onCheckedChange={(checked) => {
+                                                        return checked
+                                                            ? field.onChange([...field.value, service.id])
+                                                            : field.onChange(
+                                                                field.value?.filter(
+                                                                (value) => value !== service.id
+                                                                )
+                                                            )
+                                                        }}
+                                                    />
+                                                    </FormControl>
+                                                    <FormLabel className="font-normal">
+                                                        {service.label}
+                                                        </FormLabel>
+                                                </FormItem>
+                                            )
+                                        }}
+                                    />
+                                ))}
+                                 <FormMessage />
+                            </FormItem>
+                        )} /> 
+                     <FormField control={form.control} name="value" render={({field}) =>(
+                            <FormItem className="mt-3">
+                                <FormLabel>Value</FormLabel>
+                                <FormControl>
+                                <Input placeholder="value" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )} />
+                    <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                            <Button variant="outline" onClick={clientModal.onClose}>Cancel</Button>
+                            <Button type="submit">Save Client</Button>
+                     </div>
+          </form>
+      </Form>
+    </div>
   )
 }
 
